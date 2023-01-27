bd93c98b0a10e7aa9d8bbbaca233c2b3de5d2e79
Bansharee Ireen <81589236+banshee56@users.noreply.github.com>
2023-01-27 17:58:32 -0500

    finished load_dataset

--- load_dataset.py
+++ load_dataset.py
@@ -16,12 +16,25 @@ def load_dataset(dataset_path, mean_subtraction, normalization):
     -------
     train_ds (TensorDataset): The features and their corresponding labels bundled as a dataset
     """
-    # Load the dataset and extract the features and the labels
+    # Load the dataset and extract the features and the labels    
+    file = torch.load(dataset_path)
+    features = file['features']
+    labels = file['labels']
 
     # Do mean_subtraction if it is enabled
+    if mean_subtraction:
+        mean_per_feature = torch.mean(features, axis=0)         # mean per column
+        features = torch.subtract(features, mean_per_feature)   # subtracts training set mean from each example in the set
 
     # do normalization if it is enabled
+    if normalization:
+        std_per_feature = torch.std(features, axis=0)           # std per column
+        # if std = 0 for a feature, normalization step for that feature has to be skipped, i.e. divide feature by 1
+        std_per_feature[std_per_feature == 0] = 1              
+        features = torch.divide(features, std_per_feature)      # divide training set by the per-feature standard devations
 
     # create tensor dataset train_ds
-
+    train_ds = TensorDataset(features, labels)
     return train_ds
+
+# load_dataset("iris_dataset.pt", True, True)
--- t.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-loss = torch.nn.MSELoss()
-input = torch.randn(3, 5, requires_grad=True)
-target = torch.randn(3, 5)
-output = loss(input, target)
-print(output.forward())
